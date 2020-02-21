92afc24e7ec8ae10eb519cc2f5c3ab7911ec8c96
Andrey Volodin <Andrey_Volodin@epam.com>
2020-02-22 02:30:49 +0300

    Don't call stopGame() on every text message
    Move try-finally block to "create new game and wait for end" branch

--- bot.mjs
+++ bot.mjs
@@ -6,37 +6,42 @@ export default {
   start: () => {
     const bot = new TelegramBot(token, {polling: true});
     bot.onText(/^го/i, async (message, match) => {
-      try {
-        const chatId = message.chat.id;
-        const game = gameController.getState();
-        if (game.inProgress) {
-          if (game.url) {
-            bot.sendMessage(chatId, `Уже играем! ${game.url}`);
-          } else {
-            bot.sendMessage(chatId, 'Создаю, пришлю ссылку как сделаю! (Может быть)');
-          }
+      const chatId = message.chat.id;
+      const game = gameController.getState();
+
+      if (game.inProgress) {
+        if (game.url) {
+          bot.sendMessage(chatId, `Уже играем! ${game.url}`);
         } else {
-          bot.sendMessage(chatId, `Ща создам игру на ${game.gameTime} мин`);
-          const url = await gameController.startGame();
-          if (!!url) {
-            bot.sendMessage(chatId, url);
-          } else {
-            bot.sendMessage(chatId, 'Не получилось создать игру. Попробуйте позже.');
-            throw new Error(`[startGame] didn't return url`);
-          }
-
-          const result = await gameController.waitEndGame();
-          if (!!result) {
-            bot.sendMessage(chatId, result);
-          }
-
-          await gameController.stopGame();
-          bot.sendMessage(chatId, 'Игра закончена.');
+          bot.sendMessage(chatId, 'Создаю, пришлю ссылку как сделаю! (Может быть)');
         }
-      } catch (e) {
-        console.error(e.message);
-      } finally {
+      } else {
+        try {
+          await createAndWaitForGameEnd();
+        } catch (e) {
+          console.error(e.message);
+        } finally {
+          await gameController.stopGame();
+        }
+      }
+
+      async function createAndWaitForGameEnd() {
+        bot.sendMessage(chatId, `Ща создам игру на ${game.gameTime} мин`);
+        const url = await gameController.startGame();
+        if (!!url) {
+          bot.sendMessage(chatId, url);
+        } else {
+          bot.sendMessage(chatId, 'Не получилось создать игру. Попробуйте позже.');
+          throw new Error(`[startGame] didn't return url`);
+        }
+
+        const result = await gameController.waitEndGame();
+        if (!!result) {
+          bot.sendMessage(chatId, result);
+        }
+
         await gameController.stopGame();
+        bot.sendMessage(chatId, 'Игра закончена.');
       }
     });
     return Promise.resolve();
