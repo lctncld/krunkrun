11a47db76caa479f9b14e5dbf9215a39d52b2961
santonov62 <flex62ryz@ya.ru>
2020-02-21 17:37:03 +0300

    Result statistics

--- index.js
+++ index.js
@@ -2,64 +2,129 @@ const puppeteer = require('puppeteer');
 
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.KRUN_KRUN_TOKEN;
+const gameTime = process.env.GAME_TIME || 12;
 
 const game = {
   timeLimit: 1, // minutes
   inProgress: false,
   url: void 0
 };
+let browser;
 
 (async () => {
-  const bot = new TelegramBot(token, {polling: true});
-  bot.onText(/го/, async (message, match) => {
-    const chatId = message.chat.id;
+  
+  try {
+    const bot = new TelegramBot(token, {polling: true});
+// bot.on("polling_error", (msg) => console.log(msg));
+    bot.onText(/го/, async (message, match) => {
+      const chatId = message.chat.id;
 
-    if (game.inProgress) {
-      if (game.url) {
-        bot.sendMessage(chatId, `Уже играем! ${game.url}`);
+      if (game.inProgress) {
+        if (game.url) {
+          bot.sendMessage(chatId, `Уже играем! ${game.url}`);
+        } else {
+          bot.sendMessage(chatId, 'Создаю, пришлю ссылку как сделаю! (Может быть)');
+        }
       } else {
-        bot.sendMessage(chatId, 'Создаю, пришлю ссылку как сделаю! (Может быть)');
+        bot.sendMessage(chatId, `Ща создам игру на ${gameTime} мин`);
+        const url = await startGame();
+        game.url = url;
+        if (!!url) {
+          bot.sendMessage(chatId, url);
+        } else {
+          bot.sendMessage(chatId, 'Не получилось создать игру. Попробуйте позже.');
+           throw new Error(`[startGame] doesn't return url`);
+        }
+        
+        const result = await waitEndGame();
+        if (!!result)
+          bot.sendMessage(chatId, result);
+
+        await stopGame();
+        bot.sendMessage(chatId, 'Игра закончена');
+
       }
-    } else {
-      bot.sendMessage(chatId, 'Ща создам');
-      game.inProgress = true;
+    });
 
-      const browser = await puppeteer.launch({
-        headless: false,
-        args: [
-          '--app=https://www.google.com/',
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-infobars',
-          '--window-position=0,0',
-          '--ignore-certifcate-errors',
-          '--ignore-certifcate-errors-spki-list',
-        ]
-      });
-      const page = await browser.newPage();
+    bot.onText(/стоп/, async (message, match) => {
+      const chatId = message.chat.id;
+      await stopGame();
+      bot.sendMessage(chatId, 'Сервер выключен');
+    });
 
-      await hideFromBotDetector(page);
-      game.url = await createGame(page);
-      bot.sendMessage(chatId, game.url);
-
-      setTimeout(async () => {
-        bot.sendMessage(chatId, 'А теперь работать!');
-        await stopHost(browser)
-      }, (game.timeLimit + 1) * 1000 * 60);
-
-      await page.waitForSelector('#instructions > span:nth-child(1)', {timeout: 0});
-      console.log('RIP server');
-      bot.sendMessage(chatId, 'Сервер отвалился, расходимся');
-      await stopHost(browser);
-    }
-  });
+  } catch (e) {
+    console.error(e.message);
+  }
+  
 })();
 
-async function stopHost(browser) {
-  console.log('stop host');
-  await browser.close();
-  game.inProgress = false;
-  game.url = void 0;
+async function stopGame() {
+  console.group("[stopGame]");
+  try {
+    game.inProgress = false;
+    browser && browser.close();
+    console.log('done');
+  } catch (e) {
+    console.error(e.message);
+  } finally {
+    console.groupEnd();
+  }
+}
+
+async function waitEndGame() {
+  console.group("[waitEndGame]");
+  try {
+    const page = (await browser.pages()).find(page => page.url() === game.url);
+    console.log('...');
+    await page.waitFor(60000 * gameTime);
+    await page.waitFor('#endTable', {timeout: 30000});
+    const result = await page.evaluate(() => document.querySelector('#endTable').textContent);
+    console.log(result);
+    console.log('done');
+    return result;
+  } finally {
+    console.groupEnd();
+  }
+}
+
+async function startGame(attempts = 0) {
+  ++attempts;
+  console.group(`[startGame] attempt ${attempts}`);
+  game.inProgress = true;
+  
+  browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-infobars',
+      '--window-position=0,0',
+      '--ignore-certifcate-errors',
+      '--ignore-certifcate-errors-spki-list',
+    ]
+  });
+  
+  browser.on('disconnected', () => {
+    console.log('Puppeteer disconnected');
+    game.inProgress = false;
+  });
+  
+  try {
+    const page = await browser.newPage();
+    
+    await hideFromBotDetector(page);
+    return await createGame(page);  // game url
+    
+  } catch (e) {
+    console.error(e.message);
+    game.inProgress = false;
+    await browser.close();
+    if (attempts < 5) {
+      return await startGame(attempts);
+    }
+  } finally {
+    console.groupEnd();
+  }
 }
 
 async function hideFromBotDetector(page) {
@@ -68,7 +133,7 @@ async function hideFromBotDetector(page) {
     delete newProto.webdriver;
     navigator.__proto__ = newProto;
   });
-
+  
   await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) "
       + "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36");
 }
@@ -76,43 +141,41 @@ async function hideFromBotDetector(page) {
 async function createGame(page) {
   console.log('goto index');
   await page.goto('https://krunker.io/', {waitUntil: 'networkidle2'});
-
+  
   console.log('wait for #menuBtnHost');
   await page.waitFor('#menuBtnHost');
-
+  
   console.log('call openHostWindow()');
   await page.evaluate(() => openHostWindow());
   // await page.click('#menuBtnHost');
-
+  
   const maps = {
     littletown: '#menuWindow > div:nth-child(3) > label:nth-child(2) > div.hostMapName.blackShad'
   };
   console.log('wait for game options');
   await page.waitFor(maps.littletown);
-
+  
   console.log('select map and set game params');
   await page.click(maps.littletown);
-
+  
   await page.evaluate(() => document.querySelector('#customSmaxPlayers').value = 10);
-  await page.evaluate(() => document.querySelector('#customSgameTime').value = 10);
+  await page.evaluate(gameTime => document.querySelector('#customSgameTime').value = gameTime, gameTime);
   await page.evaluate(() => document.querySelector('#makePrivate').checked = true);
-
+  
   console.log('create room');
   // await page.click('#menuWindow > div:nth-child(5) > div:nth-child(16) > div:nth-child(44) > a');
   await page.evaluate(() => createPrivateRoom());
-
+  
   console.log('wait for url change');
-  try {
-    await page.waitForNavigation({waitUntil: 'networkidle2', timeout: 3000});
+  await page.waitFor(() => document.querySelector('#hostGameMsg').textContent !== 'Please wait...');
+  
+  const serverMessage = await page.$eval('#hostGameMsg', element => element.textContent);
+  console.log('hostGameMsg ', serverMessage);
+  
+  if (serverMessage.indexOf('Success') >= 0) {
     const url = await page.url();
     console.log(`---> ${url}`);
     return url;
-  } catch (e) {
-    const serverMessage = await page.$eval('#hostGameMsg', element => element.textContent);
-    console.log(`server message: ${serverMessage}`);
-    if (serverMessage === 'Server limit reached. Try on a different Server') {
-      console.log('reload and create game again');
-      await createGame(page);
-    }
   }
+  throw new Error('Error create game');
 }
